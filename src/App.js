638ccf90b9349b8626b46d01de7064635a30a595
morinokotoba <kaorisasada39@gmail.com>
2025-04-09 23:06:11 +0900

    Update App.js
    Update welcome text

--- src/App.js
+++ src/App.js
@@ -4,22 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ようこそ、森の言葉アートサイトへ！🌲✨</h1>
     </div>
   );
 }
 
 export default App;
+
