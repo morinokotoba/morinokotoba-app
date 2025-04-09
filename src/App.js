88111849d62220ef51dbdec236aed8a4bd2921fa
morinokotoba <kaorisasada39@gmail.com>
2025-04-10 07:16:34 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+function Home() {
+  return <h1>ようこそ、森の言葉アートサイトへ！🌲✨</h1>;
+}
+
+function About() {
+  return <h1>このサイトについて</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>ようこそ、森の言葉アートサイトへ！🌲✨</h1>
-    </div>
+    <Router>
+      <nav style={{ padding: "1rem" }}>
+        <Link to="/" style={{ marginRight: "1rem" }}>ホーム</Link>
+        <Link to="/about">このサイトについて</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
